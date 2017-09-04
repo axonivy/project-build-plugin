49fc2edd1a11fd48b900a6c25c458c638fe4ebd2
Alexander Suter <alexander.suter@axonivy.com>
2017-09-04 14:35:55 +0200

    Increase DEFAULT_VERSION from 6.7.0 to 6.7.1

--- src/main/java/ch/ivyteam/ivy/maven/AbstractEngineMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/AbstractEngineMojo.java
@@ -37,7 +37,7 @@ public abstract class AbstractEngineMojo extends AbstractMojo
 {
   /** keep synch with pom.xml &gt; reporting &gt; maven-plugin-plugin &gt; requirements */
   protected static final String MINIMAL_COMPATIBLE_VERSION = "6.5.1";
-  protected static final String DEFAULT_VERSION = "6.7.0";
+  protected static final String DEFAULT_VERSION = "6.7.1";
   
   protected static final String ENGINE_DIRECTORY_PROPERTY = "ivy.engine.directory";
   
