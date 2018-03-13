7d8966ada6632c3e7ce1c3504064d0d7911e10f8
Reguel <reguel.wermelinger@ivyteam.ch>
2018-03-13 14:13:59 +0100

    document 'PACKED' as possible target file format

--- src/main/java/ch/ivyteam/ivy/maven/DeployToEngineMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/DeployToEngineMojo.java
@@ -178,6 +178,7 @@ public class DeployToEngineMojo extends AbstractEngineMojo
    * <ul>
    *    <li><code>AUTO</code>: Keep the format of the origin project file if possible. Deploys IAR or ZIP projects into a ZIP process model version. <br>
    *        But if the target PMV already exists as expanded directory, the new version will be expanded as well.</li>
+   *    <li><code>PACKED</code>: Enforce the deployment of a project as zipped file. Normal (expanded) project directories will be compressed into a ZIP during deployment.</li>
    *    <li><code>EXPANDED</code>: Enforce the deployment of a project as expanded file directory.<br>
    *        This is recommended for projects that change the project files at runtime. E.g. projects that use the Content Management (CMS) write API.<br>
    *        The expanded format behaves exactly like projects deployed with Axon.ivy 7.0 or older. You might choose to deploy expanded projects in order to avoid {@link ReadOnlyFileSystemException} at runtime.<br>
