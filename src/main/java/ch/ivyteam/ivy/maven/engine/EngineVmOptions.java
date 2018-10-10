07e1fb4b57dd22e9be55f4bbe0b60e3127dbd780
Alexander Suter <alexander.suter@axonivy.com>
2018-10-10 08:05:40 +0200

    XIVY-2606 Change name of organisation from AXON IVY AG to AXON Ivy AG

--- src/main/java/ch/ivyteam/ivy/maven/engine/EngineVmOptions.java
+++ src/main/java/ch/ivyteam/ivy/maven/engine/EngineVmOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 AXON IVY AG
+ * Copyright (C) 2018 AXON Ivy AG
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
