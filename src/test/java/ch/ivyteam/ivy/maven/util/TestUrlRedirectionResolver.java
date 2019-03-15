cb7081fded02b726ea23c4f5ff6edfd44f1342ec
ivy-arus <andreas.rusch@axonivy.com>
2019-03-15 15:16:38 +0100

    Fix test

--- src/test/java/ch/ivyteam/ivy/maven/util/TestUrlRedirectionResolver.java
+++ src/test/java/ch/ivyteam/ivy/maven/util/TestUrlRedirectionResolver.java
@@ -26,7 +26,7 @@ public class TestUrlRedirectionResolver
   @Test
   public void redirections() throws IOException
   {
-    assertRedirection("http://developer.axonivy.com/download", "http://developer.axonivy.com/download", "https://developer.axonivy.com/download");
+    assertRedirection("http://answers.axonivy.com/", "http://answers.axonivy.com/", "https://answers.axonivy.com/");
   }
 
   private static void assertRedirection(String initUrl, String... openendUrls) throws IOException
