a2815f909769eab0540d308713461cfcd1bae4f9
Christian Strebel <christian.strebel@axonivy.com>
2019-07-08 10:01:22 +0200

    XIVY-3171 Set default version to 7.4.0

--- src/main/java/ch/ivyteam/ivy/maven/AbstractEngineMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/AbstractEngineMojo.java
@@ -37,7 +37,7 @@ public abstract class AbstractEngineMojo extends AbstractMojo
 {
   /** keep synch with pom.xml &gt; reporting &gt; maven-plugin-plugin &gt; requirements */
   protected static final String MINIMAL_COMPATIBLE_VERSION = "7.3.0";
-  protected static final String DEFAULT_VERSION = "7.3.2";
+  protected static final String DEFAULT_VERSION = "7.4.0";
   
   protected static final String ENGINE_DIRECTORY_PROPERTY = "ivy.engine.directory";
   
