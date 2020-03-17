0a24bfbb0fe36d3745b069cc6e06e54ce17a62d7
Oliver <42733117+ivy-ohe@users.noreply.github.com>
2020-03-17 15:15:26 +0100

    XIVY-2275 Make stop engine mojo null safe (#68)
    * XIVY-2275 Make stop engine mojo null safe

---
+++ src/test/java/ch/ivyteam/ivy/maven/TestEngineControlEngineDirectory.java
@@ -0,0 +1,40 @@
+package ch.ivyteam.ivy.maven;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import ch.ivyteam.ivy.maven.AbstractIntegrationTestMojo.TestEngineLocation;
+import ch.ivyteam.ivy.maven.engine.EngineControl;
+import ch.ivyteam.ivy.maven.log.LogCollector;
+
+public class TestEngineControlEngineDirectory extends BaseEngineProjectMojoTest
+{
+  @Rule
+  public RunnableEngineMojoRule<StopTestEngineMojo> rule = new RunnableEngineMojoRule<StopTestEngineMojo>(StopTestEngineMojo.GOAL);
+  
+  @Test
+  public void engineControl_engineDir_doesNotExist() throws Exception
+  {
+    LogCollector log = new LogCollector();
+    StopTestEngineMojo mojo = rule.getMojo();
+    mojo.setLog(log);
+    mojo.testEngine = TestEngineLocation.MODIFY_EXISTING;
+    
+    EngineControl controller = mojo.createEngineController();
+    controller.stop();
+    assertThat(log.getWarnings()).isEmpty();
+    assertThat(log.getErrors()).isEmpty();
+  }
+  
+  @Test
+  public void engineControl_engineDir_isNull() throws Exception
+  {
+    StopTestEngineMojo mojo = new StopTestEngineMojo();
+    mojo.project = rule.project;
+    mojo.engineDirectory = rule.getMojo().engineCacheDirectory;
+    
+    assertThat(mojo.createEngineController()).isNotNull();
+  }
+}
