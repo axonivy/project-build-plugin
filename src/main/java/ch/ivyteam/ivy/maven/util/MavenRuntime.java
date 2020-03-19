4afbc0dc578048e7d8550b93407b6d295f8897df
Reguel <reguel.wermelinger@axonivy.com>
2020-03-19 08:11:49 +0100

    XIVY-3565 prefer local reactor projects over IAR deps (#75)
    - local projects are up to date and never dated repo artifacts
    - local projects are available even when not packed: e.g. 'mvn clean
    test'
    - local projects (non IAR) are currently easier to deploy for
    BpmExecTests.

--- src/main/java/ch/ivyteam/ivy/maven/util/MavenRuntime.java
+++ src/main/java/ch/ivyteam/ivy/maven/util/MavenRuntime.java
@@ -6,26 +6,49 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.project.MavenProject;
 
 public class MavenRuntime
 {
   public static List<File> getDependencies(MavenProject project, String type)
   {
-    Set<org.apache.maven.artifact.Artifact> dependencies = project.getArtifacts();
+    return getDependencies(project, null, type);
+  }
+  
+  public static List<File> getDependencies(MavenProject project, MavenSession session, String type)
+  {
+    Set<Artifact> dependencies = project.getArtifacts();
     if (dependencies == null)
     {
       return Collections.emptyList();
     }
     
     List<File> deps = new ArrayList<>();
-    for(org.apache.maven.artifact.Artifact artifact : dependencies)
+    for(Artifact artifact : dependencies)
     {
-      if (artifact.getType().equals(type))
+      MavenProject reactorProject = findReactorProject(session, artifact);
+      if (reactorProject != null && reactorProject.getArtifact().getType().equals(type))
+      {
+        deps.add(reactorProject.getBasedir());
+      }
+      else if (artifact.getType().equals(type))
       {
         deps.add(artifact.getFile());
       }
     }
     return deps;
+  }
+
+  private static MavenProject findReactorProject(MavenSession session, Artifact artifact)
+  {
+    if (session == null)
+    {
+      return null;
+    }
+    String artifactKey = artifact.getGroupId()+":"+artifact.getArtifactId()+":"+artifact.getVersion();
+    MavenProject reactorProject = session.getProjectMap().get(artifactKey);
+    return reactorProject;
   }
 }
