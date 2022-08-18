bb85c2a10ccf61ecff60fc841210bae967721a89
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2022-08-18 16:39:05 +0200

    XIVY-8025 refactor: do not share collecting logic
    - prefer clear readable transitions

--- src/main/java/ch/ivyteam/ivy/maven/util/MavenDependencies.java
+++ src/main/java/ch/ivyteam/ivy/maven/util/MavenDependencies.java
@@ -30,21 +30,26 @@ public class MavenDependencies {
   }
 
   public List<File> localTransient() {
-    Stream<Artifact> artifacts = stream(project.getArtifacts())
-            .filter(this::isLocalDep);
-    return getFiles(artifacts);
+    return stream(project.getArtifacts())
+      .filter(this::isLocalDep)
+      .filter(this::include)
+      .map(this::toFile)
+      .collect(Collectors.toList());
   }
 
   private boolean isLocalDep(Artifact artifact) {
     return artifact.getDependencyTrail().stream()
-            .filter(dep -> dep.contains(":iar")) // iar or iar-integration-test
-            .filter(dep -> !dep.startsWith(project.getGroupId() + ":" + project.getArtifactId() + ":"))
-            .findAny()
-            .isEmpty();
+      .filter(dep -> dep.contains(":iar")) // iar or iar-integration-test
+      .filter(dep -> !dep.startsWith(project.getGroupId() + ":" + project.getArtifactId() + ":"))
+      .findAny()
+      .isEmpty();
   }
 
   public List<File> all() {
-    return getFiles(stream(project.getArtifacts()));
+    return stream(project.getArtifacts())
+      .filter(this::include)
+      .map(this::toFile)
+      .collect(Collectors.toList());
   }
 
   private static Stream<Artifact> stream(Set<Artifact> deps) {
@@ -54,17 +59,10 @@ public class MavenDependencies {
     return deps.stream();
   }
 
-  private List<File> getFiles(Stream<Artifact> dependencies) {
-    return dependencies
-            .filter(this::include)
-            .map(this::toFile)
-            .collect(Collectors.toList());
-  }
-
   private File toFile(Artifact artifact) {
     return findReactorProject(artifact)
-            .map(MavenProject::getBasedir)
-            .orElse(artifact.getFile());
+      .map(MavenProject::getBasedir)
+      .orElse(artifact.getFile());
   }
 
   private boolean include(Artifact artifact) {
