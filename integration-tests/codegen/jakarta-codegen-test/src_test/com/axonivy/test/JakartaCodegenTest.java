8714c8d927b97bc05aeca1811e061153e8946cbd
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-07-21 16:53:23 +0200

    Revert "XIVY-4974 disable for now; still we get compile time verification"
    This reverts commit ff0d2255cacdc774e902b17101a3c3948a3ae3bf.

--- integration-tests/codegen/jakarta-codegen-test/src_test/com/axonivy/test/JakartaCodegenTest.java
+++ integration-tests/codegen/jakarta-codegen-test/src_test/com/axonivy/test/JakartaCodegenTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.hibernate.query.Query;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.test.jakarta.codegen.Car;
@@ -15,7 +14,6 @@ import com.axonivy.test.jakarta.codegen._Car;
 import ch.ivyteam.ivy.environment.IvyTest;
 
 @IvyTest
-@Disabled("runtime PMV resolution issues")
 class JakartaCodegenTest {
 
   @Test
