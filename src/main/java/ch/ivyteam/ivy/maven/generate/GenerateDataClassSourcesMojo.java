a9d61d1cd68039ac473bb6959247d05b7e13f5ff
Louis <louis.mueller@axonivy.com>
2025-08-22 10:37:13 +0200

    XIVY-17305 doc for generate source (#772)

--- src/main/java/ch/ivyteam/ivy/maven/generate/GenerateDataClassSourcesMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/generate/GenerateDataClassSourcesMojo.java
@@ -6,6 +6,30 @@ import org.apache.maven.plugins.annotations.Parameter;
 import ch.ivyteam.ivy.maven.compile.AbstractEngineInstanceMojo;
 import ch.ivyteam.ivy.maven.engine.MavenProjectBuilderProxy;
 
+/**
+ * <p>
+ * Generates Data Class Java source files.
+ * </p>
+ *
+ * <p>
+ * Command line invocation is supported.
+ * </p>
+ *
+ * <pre>
+ * mvn com.axonivy.ivy.ci:project-build-plugin:generate-data-class-sources
+ * </pre>
+ *
+ * <p>
+ * Source generation can be skipped using:
+ * </p>
+ *
+ * <pre>
+ * mvn com.axonivy.ivy.ci:project-build-plugin:generate-data-class-sources
+ * -Divy.generate.data.class.sources.skip=true
+ * </pre>
+ *
+ * @since 13.2.0
+ */
 @Mojo(name = GenerateDataClassSourcesMojo.GOAL)
 public class GenerateDataClassSourcesMojo extends AbstractEngineInstanceMojo {
   public static final String GOAL = "generate-data-class-sources";
--- src/main/java/ch/ivyteam/ivy/maven/generate/GenerateWebServiceSourcesMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/generate/GenerateWebServiceSourcesMojo.java
@@ -6,6 +6,30 @@ import org.apache.maven.plugins.annotations.Parameter;
 import ch.ivyteam.ivy.maven.compile.AbstractEngineInstanceMojo;
 import ch.ivyteam.ivy.maven.engine.MavenProjectBuilderProxy;
 
+/**
+ * <p>
+ * Generates Web Services Process source files.
+ * </p>
+ *
+ * <p>
+ * Command line invocation is supported.
+ * </p>
+ *
+ * <pre>
+ * mvn com.axonivy.ivy.ci:project-build-plugin:generate-web-service-sources
+ * </pre>
+ *
+ * <p>
+ * Source generation can be skipped using:
+ * </p>
+ *
+ * <pre>
+ * mvn com.axonivy.ivy.ci:project-build-plugin:generate-web-service-sources
+ * -Divy.generate.web.service.sources.skip=true
+ * </pre>
+ *
+ * @since 13.2.0
+ */
 @Mojo(name = GenerateWebServiceSourcesMojo.GOAL)
 public class GenerateWebServiceSourcesMojo extends AbstractEngineInstanceMojo {
   public static final String GOAL = "generate-web-service-sources";
