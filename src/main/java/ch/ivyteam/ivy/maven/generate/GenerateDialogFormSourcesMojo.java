6597a8c465da9747f4f1030c2636a66fc60321fa
Louis <louis.mueller@axonivy.com>
2025-08-22 09:44:30 +0200

    XIVY-17305 doc for generate source

--- src/main/java/ch/ivyteam/ivy/maven/generate/GenerateDialogFormSourcesMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/generate/GenerateDialogFormSourcesMojo.java
@@ -6,6 +6,30 @@ import org.apache.maven.plugins.annotations.Parameter;
 import ch.ivyteam.ivy.maven.compile.AbstractEngineInstanceMojo;
 import ch.ivyteam.ivy.maven.engine.MavenProjectBuilderProxy;
 
+/**
+ * <p>
+ * Generates Dialog Form source files.
+ * </p>
+ *
+ * <p>
+ * Command line invocation is supported.
+ * </p>
+ *
+ * <pre>
+ * mvn com.axonivy.ivy.ci:project-build-plugin:generate-dialog-form-sources
+ * </pre>
+ *
+ * <p>
+ * Source generation can be skipped using:
+ * </p>
+ *
+ * <pre>
+ * mvn com.axonivy.ivy.ci:project-build-plugin:generate-dialog-form-sources
+ * -Divy.generate.dialog.form.sources.skip=true
+ * </pre>
+ *
+ * @since 13.2.0
+ */
 @Mojo(name = GenerateDialogFormSourcesMojo.GOAL)
 public class GenerateDialogFormSourcesMojo extends AbstractEngineInstanceMojo {
   public static final String GOAL = "generate-dialog-form-sources";
