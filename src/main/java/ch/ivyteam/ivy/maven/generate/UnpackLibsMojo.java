b7821257159acdaae9a03b3e22ea90ff4efcbf7c
Louis <louis.mueller@axonivy.com>
2025-09-01 15:41:08 +0200

    XIVY-17416 introduce unpack libs mojo

--- src/main/java/ch/ivyteam/ivy/maven/generate/UnpackLibsMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/generate/UnpackLibsMojo.java
@@ -1,14 +1,19 @@
 package ch.ivyteam.ivy.maven.generate;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.DirectoryScanner;
 
 /**
- * Unpack libs to the output directory.
+ * Unpack libs to output directory.
  * By default lib/generated/rest/.*jar and lib_ws/client/*.jar are unpacked.
  *
  *
@@ -23,7 +28,7 @@ public class UnpackLibsMojo extends AbstractMojo {
   MavenProject project;
 
   public static final String[] INCLUDED_LIBS = {
-      "lib/generated/rest/.*jar",
+      "lib/generated/rest/*.jar",
       "lib_ws/client/*.jar"
   };
 
@@ -33,17 +38,56 @@ public class UnpackLibsMojo extends AbstractMojo {
   @Parameter(property = "ivy.unpack.libs.skip", defaultValue = "false")
   boolean skipUnpackLibs;
 
+  /**
+   * Define exclusions for unpacking with ANT-style.
+   *
+   *
+   * Sample:
+   * <pre>
+   * <code>&lt;unpackExcludes&gt;
+   * &nbsp;&nbsp;&lt;unpackExclude&gt;lib/generated/rest/myClient.jar&lt;/unpackExclude&gt;
+   * &lt;/unpackExcludes&gt;</code>
+   * </pre>
+   */
+  @Parameter
+  String[] unpackExcludes;
+
+  /**
+   * Define inclusions for unpacking with ANT-style.
+   * Default libs to be unpacked are specified in:
+   * {@link ch.ivyteam.ivy.maven.generate.UnpackLibsMojo#INCLUDED_LIBS}.
+   *
+   *
+   * Sample:
+   * <pre>
+   * <code>&lt;unpackIncludes&gt;
+   * &nbsp;&nbsp;&lt;unpackInclude&gt;lib/.*jar&lt;/unpackInclude&gt;
+   * &lt;/unpackIncludes&gt;</code>
+   * </pre>
+   */
+  @Parameter
+  String[] unpackIncludes;
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     if (skipUnpackLibs) {
       getLog().info("Skipping unpacking libs.");
       return;
     }
+    var scanner = new DirectoryScanner();
+    scanner.setBasedir(project.getBasedir());
+    scanner.setIncludes(ArrayUtils.addAll(INCLUDED_LIBS, unpackIncludes));
+    scanner.setExcludes(unpackExcludes);
+    scanner.scan();
     var projectDir = project.getBasedir().toPath();
-    var outDir = project.getBuild().getOutputDirectory();
-
-    // TODO Auto-generated method stub
-
+    var outDir = projectDir.resolve(project.getBuild().getOutputDirectory());
+    var libs = Arrays.stream(scanner.getIncludedFiles())
+        .map(projectDir::resolve)
+        .toList();
+    try {
+      new JarUnpacker(outDir, getLog()).unpack(libs);
+    } catch (IOException ex) {
+      throw new MojoExecutionException("Failed to unpacks libs", ex);
+    }
   }
-
 }
