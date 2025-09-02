165713a44d79705fef8ad97d76a18a8e37d3a60a
Louis <louis.mueller@axonivy.com>
2025-09-02 09:23:15 +0200

    fix compile existing project test

--- src/test/java/ch/ivyteam/ivy/maven/compile/TestCompileProjectMojo.java
+++ src/test/java/ch/ivyteam/ivy/maven/compile/TestCompileProjectMojo.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2021 Axon Ivy AG
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package ch.ivyteam.ivy.maven.compile;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -78,6 +62,10 @@ public class TestCompileProjectMojo extends BaseEngineProjectMojoTest {
     PathUtils.clean(wsProcDir);
     PathUtils.clean(dataClassDir);
 
+    assertThat(classDir)
+        .isDirectoryRecursivelyContaining("glob:**gugus.txt")
+        .isDirectoryRecursivelyContaining("glob:**ch/ivyteam/test/bla.class");
+
     mojo.buildApplicationDirectory = Files.createTempDirectory("MyBuildApplication");
     execGenerateMojo();
     mojo.execute();
@@ -85,17 +73,18 @@ public class TestCompileProjectMojo extends BaseEngineProjectMojoTest {
     assertThat(findFiles(dataClassDir, "java")).hasSize(3);
     assertThat(findFiles(wsProcDir, "java")).hasSize(1);
 
-    assertThat(findFiles(classDir, "txt"))
-        .as("classes directory must only be cleand by the builder before compilation if class files are found")
-        .hasSize(1);
+    assertThat(classDir)
+        .as("nothing is expected to be cleaned during compile phase")
+        .isDirectoryRecursivelyContaining("glob:**gugus.txt")
+        .isDirectoryRecursivelyContaining("glob:**ch/ivyteam/test/bla.class");
     assertThat(findFiles(classDir, "class"))
-        .as("compiled classes must exist. but not contain any test class or old class files.")
-        .hasSize(5);
+        .as("compiled classes must exist, as well as old class files if clean phase was not executed.")
+        .hasSize(6);
 
     testMojo.execute();
     assertThat(findFiles(classDir, "class"))
         .as("compiled classes must contain test resources as well")
-        .hasSize(6);
+        .hasSize(7);
   }
 
   private static List<Path> findFiles(Path dir, String fileExtension) throws IOException {
