8f97267654569eac194c8ed0cf1def8caa289cf0
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-11-05 19:06:13 +0100

    windows

--- src/test/java/ch/ivyteam/ivy/maven/TestLoggerIntegration.java
+++ src/test/java/ch/ivyteam/ivy/maven/TestLoggerIntegration.java
@@ -6,25 +6,20 @@ import org.apache.maven.api.plugin.testing.InjectMojo;
 import org.apache.maven.api.plugin.testing.MojoTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import ch.ivyteam.ivy.maven.compile.CompileProjectMojo;
 import ch.ivyteam.ivy.maven.engine.Slf4jSimpleEngineProperties;
 import ch.ivyteam.ivy.maven.extension.LocalRepoTest;
-import ch.ivyteam.ivy.maven.extension.SysoutExtension;
-import ch.ivyteam.ivy.maven.extension.SysoutExtension.Sysout;
+import ch.ivyteam.ivy.maven.log.LogCollector;
+import ch.ivyteam.ivy.maven.log.LogCollector.LogEntry;
 
 @MojoTest
-@ExtendWith(SysoutExtension.class)
-public class TestLoggerIntegration {
-
-  private Sysout sysout;
+class TestLoggerIntegration {
 
   @BeforeEach
-  void setup(Sysout outContent) {
+  void setup() {
     Slf4jSimpleEngineProperties.install();
     Slf4jSimpleEngineProperties.enforceSimpleConfigReload();
-    this.sysout = outContent;
   }
 
   /**
@@ -33,15 +28,17 @@ public class TestLoggerIntegration {
   @Test
   @InjectMojo(goal = CompileProjectMojo.GOAL)
   void forwardEngineLogsToMavenConsole(CompileProjectMojo compile) {
+    var logs = new LogCollector();
+    compile.setLog(logs);
     compile.localRepository = LocalRepoTest.repo();
     var engineClassLoader = compile.getEngineClassloaderFactory();
     var slf4jJars = engineClassLoader.getSlf4jJars();
     assertThat(slf4jJars).hasSize(3);
     assertThat(slf4jJars.get(0).getName().toString()).startsWith("slf4j-api-");
     compile.getLog().info("log setup");
-    assertThat(this.sysout.toString())
+    assertThat(logs.getLogs()).extracting(LogEntry::toString)
         .as("no warnings must be printed during slf4j library re-solving from local repo")
-        .isEqualToIgnoringNewLines("[INFO] log setup");
+        .containsOnly("log setup");
   }
 
 }
--- src/test/java/ch/ivyteam/ivy/maven/test/TestStartEngine.java
+++ src/test/java/ch/ivyteam/ivy/maven/test/TestStartEngine.java
@@ -17,11 +17,11 @@
 package ch.ivyteam.ivy.maven.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.maven.api.di.Provides;
 import org.apache.maven.api.plugin.testing.InjectMojo;
@@ -209,10 +209,11 @@ class TestStartEngine {
         .isExecutable();
   }
 
-  private static void kill(Process startedProcess) {
+  private static void kill(Process startedProcess) throws Exception {
     if (startedProcess != null) {
+      var exit = startedProcess.onExit();
       startedProcess.destroy();
-      await().untilAsserted(() -> assertThat(startedProcess.isAlive()).as("gracefully wait on stop").isFalse());
+      exit.get(60, TimeUnit.SECONDS);
     }
   }
 
