3ef46847186f4ba59b8b6f1b1fef37b145960098
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-12-16 14:39:34 +0100

    require engine 14 to run

--- src/main/java/ch/ivyteam/ivy/maven/AbstractEngineMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/AbstractEngineMojo.java
@@ -41,8 +41,8 @@ public abstract class AbstractEngineMojo extends AbstractMojo {
    * keep synch with pom.xml &gt; reporting &gt; maven-plugin-plugin &gt;
    * requirements
    */
-  protected static final String MINIMAL_COMPATIBLE_VERSION = "13.2.0";
-  protected static final String DEFAULT_VERSION = "13.2.0";
+  protected static final String MINIMAL_COMPATIBLE_VERSION = "14.0.0";
+  protected static final String DEFAULT_VERSION = "14.0.0";
 
   protected static final String ENGINE_DIRECTORY_PROPERTY = "ivy.engine.directory";
 
