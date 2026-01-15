236b148b98f3df18d307a3fde8d7aa66e4164a8e
Louis <louis.mueller@axonivy.com>
2026-01-15 10:24:13 +0100

    XIVY-17857 generate ws source without engine

--- src/main/java/ch/ivyteam/ivy/maven/generate/GenerateWebServiceSourcesMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/generate/GenerateWebServiceSourcesMojo.java
@@ -1,10 +1,19 @@
 package ch.ivyteam.ivy.maven.generate;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.DirectoryScanner;
 
-import ch.ivyteam.ivy.maven.compile.AbstractEngineInstanceMojo;
-import ch.ivyteam.ivy.maven.engine.MavenProjectBuilderProxy;
+import ch.ivyteam.ivy.IvyConstants;
+import ch.ivyteam.ivy.process.io.JsonProc;
+import ch.ivyteam.ivy.process.model.WebserviceProcess;
+import ch.ivyteam.ivy.webservice.process.restricted.WebserviceSourceGenerator;
 
 /**
  * <p>
@@ -31,7 +40,7 @@ import ch.ivyteam.ivy.maven.engine.MavenProjectBuilderProxy;
  * @since 13.2.0
  */
 @Mojo(name = GenerateWebServiceSourcesMojo.GOAL)
-public class GenerateWebServiceSourcesMojo extends AbstractEngineInstanceMojo {
+public class GenerateWebServiceSourcesMojo extends AbstractMojo {
   public static final String GOAL = "generate-web-service-sources";
 
   /**
@@ -41,12 +50,37 @@ public class GenerateWebServiceSourcesMojo extends AbstractEngineInstanceMojo {
   @Parameter(property = "ivy.generate.web.service.sources.skip", defaultValue = "false")
   boolean skipGenerateSources;
 
+  @Parameter(property = "project", required = true, readonly = true)
+  MavenProject project;
+
+  private static final String[] INCLUDEDS = {IvyConstants.DIRECTORY_PROCESSES + "/**/*." + IvyConstants.PROCESS_EXTENSION};
+
   @Override
-  public void engineExec(MavenProjectBuilderProxy projectBuilder) throws Exception {
+  public void execute() {
     if (skipGenerateSources) {
       return;
     }
     getLog().info("Generating Ivy web service process sources...");
-    projectBuilder.generateSources(project.getBasedir().toPath(), "WebServiceProcessSourceGenerator");
+    var jsonFiles = processFiles();
+    var projectDir = project.getBasedir().toPath();
+    var writer = new NioSourceWriter(projectDir);
+    for (var jsonFile : jsonFiles) {
+      try (var is = Files.newInputStream(projectDir.resolve(jsonFile))) {
+        var process = JsonProc.read("srcGenProcess", is);
+        if (process instanceof WebserviceProcess wsProcess) {
+          WebserviceSourceGenerator.generate(wsProcess, writer);
+        }
+      } catch (IOException ex) {
+        throw new UncheckedIOException(ex);
+      }
+    }
+  }
+
+  private String[] processFiles() {
+    var scanner = new DirectoryScanner();
+    scanner.setBasedir(project.getBasedir());
+    scanner.setIncludes(INCLUDEDS);
+    scanner.scan();
+    return scanner.getIncludedFiles();
   }
 }
