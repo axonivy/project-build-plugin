ca863c21d0f07997aa487c3740054a901f8f97ad
Alexander Suter <alexander.suter@axonivy.com>
2026-04-20 09:54:24 +0200

    Improve Project Validation

--- src/test/java/ch/ivyteam/ivy/maven/extension/LocalRepoTest.java
+++ src/test/java/ch/ivyteam/ivy/maven/extension/LocalRepoTest.java
@@ -3,16 +3,8 @@ package ch.ivyteam.ivy.maven.extension;
 import java.nio.file.Path;
 
 import org.apache.commons.lang3.SystemUtils;
-import org.apache.maven.artifact.repository.MavenArtifactRepository;
-import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
 
 public class LocalRepoTest {
-
-  public static MavenArtifactRepository repo() {
-    var layout = new DefaultRepositoryLayout();
-    String string = path().toUri().toString();
-    return new MavenArtifactRepository("user", string, layout, null, null);
-  }
 
   public static Path path() {
     String repoOverride = System.getProperty("maven.repo.local");
@@ -22,5 +14,4 @@ public class LocalRepoTest {
     var home = SystemUtils.getUserHomePath();
     return home.resolve(".m2").resolve("repository");
   }
-
 }
