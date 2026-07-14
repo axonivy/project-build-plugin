800d3f26477036e4515fa9a673645c91b4c8db4d
Louis <louis.mueller@axonivy.com>
2026-07-14 14:22:58 +0200

    no params

--- src/main/java/ch/ivyteam/ivy/maven/AddResourceMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/AddResourceMojo.java
@@ -1,7 +1,6 @@
 package ch.ivyteam.ivy.maven;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.apache.maven.model.Resource;
@@ -16,8 +15,7 @@ import org.codehaus.plexus.util.DirectoryScanner;
 
 /**
  * Dynamically adds resource directories to the Maven build.
- * The directories are resolved using ANT-style glob patterns relative to the project base directory.
- * By default, all direct subdirectories of <code>src_generated/rest/</code> and
+ * All direct subdirectories of <code>src_generated/rest/</code> and
  * <code>src_generated/ws/</code> are added as resource directories.
  *
  * @since 14.0.0
@@ -36,20 +34,8 @@ public class AddResourceMojo extends AbstractMojo {
   @Parameter(property = "ivy.add.resource.skip", defaultValue = "false")
   boolean skipIvyAddResource;
 
-  /**
-   * ANT-style glob patterns used to scan for resource directories to add.
-   * Patterns are resolved relative to the project base directory.
-   * Each matched directory is registered as a Maven resource directory.
-   */
-  @Parameter(defaultValue = "src_generated/rest/*,src_generated/ws/*")
-  String[] ivyResourcesToAdd;
-
-  /**
-   * File patterns to exclude from each added resource directory.
-   * Applied to all directories matched by {@link #ivyResourcesToAdd}.
-   */
-  @Parameter(defaultValue = "**/*.java")
-  String[] ivyResourceExcludes;
+  private static final String[] INCLUDES = {"src_generated/rest/*", "src_generated/ws/*"};
+  private static final String EXCLUDE = "**/*.java";
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
@@ -64,7 +50,7 @@ public class AddResourceMojo extends AbstractMojo {
     var resourceDir = new File(project.getBasedir(), dir);
     var resource = new Resource();
     resource.setDirectory(resourceDir.getAbsolutePath());
-    resource.setExcludes(Arrays.asList(ivyResourceExcludes));
+    resource.addExclude(EXCLUDE);
     getLog().info("Adding resource directory: " + resource.getDirectory());
     project.addResource(resource);
   }
@@ -72,7 +58,7 @@ public class AddResourceMojo extends AbstractMojo {
   private Stream<String> resourceDirectoriesToAdd() {
     var scanner = new DirectoryScanner();
     scanner.setBasedir(project.getBasedir());
-    scanner.setIncludes(ivyResourcesToAdd);
+    scanner.setIncludes(INCLUDES);
     scanner.scan();
     return Stream.of(scanner.getIncludedDirectories());
   }
